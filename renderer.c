6cd6584675cc91be0dce27809aeab7748621e43e
Sam Anthony <sam@samanthony.xyz>
2025-02-23 22:27:33 -0500

    fix whitespace in button maps

--- renderer.c
+++ renderer.c
@@ -21,20 +21,20 @@ enum font { FONTSIZE = 14, };
 static const mu_Color bg = {255, 255, 255, 255};
 
 static const char button_map[256] = {
-  [ SDL_BUTTON_LEFT & 0xff ] = MU_MOUSE_LEFT,
-  [ SDL_BUTTON_RIGHT & 0xff ] = MU_MOUSE_RIGHT,
-  [ SDL_BUTTON_MIDDLE & 0xff ] = MU_MOUSE_MIDDLE,
+	[SDL_BUTTON_LEFT & 0xff] = MU_MOUSE_LEFT,
+	[SDL_BUTTON_RIGHT & 0xff] = MU_MOUSE_RIGHT,
+	[SDL_BUTTON_MIDDLE & 0xff] = MU_MOUSE_MIDDLE,
 };
 
 static const char key_map[256] = {
-  [ SDLK_LSHIFT & 0xff ] = MU_KEY_SHIFT,
-  [ SDLK_RSHIFT & 0xff ] = MU_KEY_SHIFT,
-  [ SDLK_LCTRL & 0xff ] = MU_KEY_CTRL,
-  [ SDLK_RCTRL & 0xff ] = MU_KEY_CTRL,
-  [ SDLK_LALT & 0xff ] = MU_KEY_ALT,
-  [ SDLK_RALT & 0xff ] = MU_KEY_ALT,
-  [ SDLK_RETURN & 0xff ] = MU_KEY_RETURN,
-  [ SDLK_BACKSPACE & 0xff ] = MU_KEY_BACKSPACE,
+	[SDLK_LSHIFT & 0xff] = MU_KEY_SHIFT,
+	[SDLK_RSHIFT & 0xff] = MU_KEY_SHIFT,
+	[SDLK_LCTRL & 0xff] = MU_KEY_CTRL,
+	[SDLK_RCTRL & 0xff] = MU_KEY_CTRL,
+	[SDLK_LALT & 0xff] = MU_KEY_ALT,
+	[SDLK_RALT & 0xff] = MU_KEY_ALT,
+	[SDLK_RETURN & 0xff] = MU_KEY_RETURN,
+	[SDLK_BACKSPACE & 0xff] = MU_KEY_BACKSPACE,
 };
 
 static void print_info(void);
