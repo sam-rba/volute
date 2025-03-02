b4fd126b2c779c800bdf3f2c4a47bd85ade9668a
Sam Anthony <sam@samanthony.xyz>
2025-03-02 14:11:48 -0500

    mass flow rate tests

--- test_mass_flow_rate.c
+++ test_mass_flow_rate.c
@@ -6,20 +6,20 @@
 
 void
 test_kilo_per_sec(void) {
-	assert(0);
+	test(kilo_per_sec(123.456), 123.456);
 }
 
 void
 test_pound_per_min(void) {
-	assert(0);
+	test(pound_per_min(123.456), 0.9333117);
 }
 
 void
 test_as_kilo_per_sec(void) {
-	assert(0);
+	test(as_kilo_per_sec(kilo_per_sec(123.456)), 123.456);
 }
 
 void
 test_as_pound_per_min(void) {
-	assert(0);
+	test(as_pound_per_min(pound_per_min(123.456)), 123.456);
 }
